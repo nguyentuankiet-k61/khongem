e49780f86b44b113f5100d27ba70b8a89fb60887
nguyentuankiet-k61 <72546259+nguyentuankiet-k61@users.noreply.github.com>
2023-12-18 11:47:12 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ var sound = new Howl({
     urls: ['Merry Christmas_song_lastchrismas.mp3'],
     loop: true
   });
+function loading() {
+    $('body').css('height', $(window).height());
+    $('#loading').css('visibility', 'visible');
+    setTimeout('loadingVisible()', 1500);
+}
+
+function loadingVisible() {
+    $('#loading').css('visibility', 'hidden');
+    $('body').css({
+        'overflow': 'visible',
+        'height': '100%'
+    });
+    sound.play();
+}
 var boxgift = document.querySelector('.box-gift')
 var Close = document.querySelector('.fa-xmark')
 var boxContent = document.querySelector('.box-content')
